d62562ef0592b9de7f75a51f73ac5e8b02142aef
ivanthengwr <ivanthengwr@gmail.com>
2025-10-01 00:59:46 +0800

    Clean up codebase

---
+++ Light_sensor_lux_example/LightSensor_Lux_example/LightSensor_Lux_example.ino
@@ -0,0 +1,49 @@
+/*******************************************************
+ * ESP32 LDR with Lux-Based LED Control
+ *******************************************************/
+
+const int LED_PIN = 6;      // LED GPIO
+const int LDR_PIN = 7;      // ADC pin
+
+// LDR characteristics (typical)
+const float GAMMA = 0.5;          // LDR gamma
+const float R_REF = 5e3;          // LDR resistance at reference lux (ohms)
+const float LUX_REF = 10.0;       // Reference lux
+
+const float VCC = 3.3;            // ESP32 supply voltage
+const float R_FIXED = 10e3;       // Fixed resistor in voltage divider (ohms)
+
+void setup() {
+    Serial.begin(115200);
+    ledcAttach(LED_PIN, 5000, 8);
+    Serial.println("ESP32 LDR Lux Test");
+}
+
+void loop() {
+    // Read ADC value (0–4095)
+    int adc = analogRead(LDR_PIN);
+
+    // Convert ADC to voltage
+    float voltage = (adc / 4095.0) * VCC;
+
+    // Calculate LDR resistance
+    float rLDR = R_FIXED * voltage / (VCC - voltage);
+
+    // Convert resistance to lux
+    float lux = LUX_REF * pow(R_REF / rLDR, 1.0 / GAMMA);
+
+    // Print values for monitoring
+    Serial.print("ADC: "); Serial.print(adc);
+    Serial.print("\tVoltage: "); Serial.print(voltage, 3);
+    Serial.print(" V\tR_LDR: "); Serial.print(rLDR, 0);
+    Serial.print(" Ω\tLux: "); Serial.println(lux, 1);
+
+    // LED logic based on lux
+    if (lux < 25.0) {          // Dark threshold (adjustable)
+      ledcWrite(LED_PIN, 200);   // LED ON
+    } else {
+      ledcWrite(LED_PIN, 0);    // LED OFF
+    }
+
+    delay(500);
+}
--- RainMaker_example/RainMaker_example.ino
+++ RainMaker_example/RainMaker_example.ino
@@ -3,7 +3,7 @@
 #include "WiFiProv.h"
 #include "AppInsights.h"
 
-const char *service_name = "PROV_1234";
+const char *service_name = "PROV_ivanthengwr"; // Use a unique service_name keep the PROV_ prefix
 const char *pop = "abcd1234";
 
 #if CONFIG_IDF_TARGET_ESP32C3
@@ -12,16 +12,16 @@ static int inbuilt_led_pin = 8;
 static int inbuilt_led_pin = 48;
 #endif
 
-static int led_1_pin = 5;
-static int ldr_sensor_pin = 6;
+static int led_1_pin = 6;
+static int ldr_sensor_pin = 7;
 
 /* LED characteristics */
 const int freq = 5000;     // PWM Frequency
 const int resolution = 8;  //PWM resolution (8 bits: 0-255)
 
 /* LDR characteristics */
-const float GAMMA = 0.5;     // LDR gamma
-const float R_REF = 5e3;     // LDR resistance at reference lux (ohms)
+const float GAMMA = 0.5;    // LDR gamma
+const float R_REF = 5e3;    // LDR resistance at reference lux (ohms)
 const float LUX_REF = 500;  // Reference lux
 float lux = LUX_REF;
 
@@ -40,7 +40,7 @@ static int inbuilt_switch = 0;  //Boot Button
 #define DEFAULT_POWER_STATE true
 #define DEFAULT_DUTY_CYCLE 200
 
-/* Volatile Variables*/
+/* Volatile State Variables*/
 volatile bool provision_flag_success = false;
 volatile bool switch_state_1 = true;
 volatile bool switch_state_2 = true;
@@ -59,19 +59,14 @@ static LightBulb *inbuilt_led_2 = NULL;
 static Device *ldr_sensor = NULL;
 
 unsigned long lastReportTime = 0;
-const unsigned long reportInterval = 5000; // Report every 5 seconds
+const unsigned long reportInterval = 5000;  // Report every 5 seconds
 
 // WARNING: sysProvEvent is called from a separate FreeRTOS task (thread)!
 void sysProvEvent(arduino_event_t *sys_event) {
   switch (sys_event->event_id) {
     case ARDUINO_EVENT_PROV_START:
-#if CONFIG_IDF_TARGET_ESP32S2
-      Serial.printf("\nProvisioning Started with name \"%s\" and PoP \"%s\" on SoftAP\n", service_name, pop);
-      WiFiProv.printQR(service_name, pop, "softap");
-#else
       Serial.printf("\nProvisioning Started with name \"%s\" and PoP \"%s\" on BLE\n", service_name, pop);
       WiFiProv.printQR(service_name, pop, "ble");
-#endif
       break;
     case ARDUINO_EVENT_PROV_INIT: WiFiProv.disableAutoStop(10000); break;
     case ARDUINO_EVENT_PROV_CRED_SUCCESS:
@@ -110,132 +105,132 @@ void write_callback(Device *device, Param *param, const param_val_t val, void *p
 }
 
 float lux_handler(int ldr_pin) {
-  // Read ADC value (0–4095)
-  int adc = analogRead(ldr_pin);
-  // Convert ADC to voltage
-  float voltage = (adc / 4095.0) * VCC;
-  // Calculate LDR resistance
-  float rLDR = R_FIXED * voltage / (VCC - voltage);
-  // Convert resistance to lux
-  float lux = LUX_REF * pow(R_REF / rLDR, 1.0 / GAMMA);
-  return lux;
+    // Read ADC value (0–4095)
+    int adc = analogRead(ldr_pin);
+    // Convert ADC to voltage
+    float voltage = (adc / 4095.0) * VCC;
+    // Calculate LDR resistance
+    float rLDR = R_FIXED * voltage / (VCC - voltage);
+    // Convert resistance to lux
+    float lux = LUX_REF * pow(R_REF / rLDR, 1.0 / GAMMA);
+    return lux;
 }
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(inbuilt_switch, INPUT_PULLUP);
-  ledcAttach(led_1_pin, freq, resolution);
-  ledcWrite(led_1_pin, DEFAULT_DUTY_CYCLE);
-  rgbLedWrite(inbuilt_led_pin, DEFAULT_DUTY_CYCLE, DEFAULT_DUTY_CYCLE, DEFAULT_DUTY_CYCLE);
+    Serial.begin(115200);
+    pinMode(inbuilt_switch, INPUT_PULLUP);
+    ledcAttach(led_1_pin, freq, resolution);
+    ledcWrite(led_1_pin, DEFAULT_DUTY_CYCLE);
+    rgbLedWrite(inbuilt_led_pin, DEFAULT_DUTY_CYCLE, DEFAULT_DUTY_CYCLE, DEFAULT_DUTY_CYCLE);
 
-  Node my_node;
-  my_node = RMaker.initNode("ESP RainMaker Node");
+    Node my_node;
+    my_node = RMaker.initNode("ESP RainMaker Node");
 
-  /* Initialize led_1, inbuilt_led_2, ldr_sensor devices */
-  led_1 = new LightBulb("LED_1", &led_1_pin, DEFAULT_POWER_STATE);  // Name of device, priv data that is used for callback function, default power state
-  if (!led_1) {
-    return;
-  }
+    /* Initialize led_1, inbuilt_led_2, ldr_sensor devices */
+    led_1 = new LightBulb("LED_1", &led_1_pin, DEFAULT_POWER_STATE);  // Name of device, priv data that is used for callback function, default power state
+    if (!led_1) {
+        return;
+    }
 
-  inbuilt_led_2 = new LightBulb("INBUILT_LED_2", &inbuilt_led_pin, DEFAULT_POWER_STATE);
-  if (!inbuilt_led_2) {
-    return;
-  }
+    inbuilt_led_2 = new LightBulb("INBUILT_LED_2", &inbuilt_led_pin, DEFAULT_POWER_STATE);
+    if (!inbuilt_led_2) {
+        return;
+    }
 
-  // Initialise ldr_sensor
-  ldr_sensor = new Device("BRIGHTNESS_SENSOR", ESP_RMAKER_DEVICE_TEMP_SENSOR, NULL);
-  if (!ldr_sensor) {
-    return;
-  }
+    // Initialise ldr_sensor
+    ldr_sensor = new Device("BRIGHTNESS_SENSOR", ESP_RMAKER_DEVICE_TEMP_SENSOR, NULL);
+    if (!ldr_sensor) {
+        return;
+    }
 
-  // Add brightness parameter for led_1 & inbuilt_led_2
-  Param brightness("Brightness", ESP_RMAKER_PARAM_BRIGHTNESS, value(DEFAULT_DUTY_CYCLE), PROP_FLAG_READ | PROP_FLAG_WRITE | PROP_FLAG_PERSIST);  // Or we can use a standard param addBrightnessParam(200)
-  brightness.addBounds(value(0), value(255), value(1));
-  brightness.addUIType(ESP_RMAKER_UI_SLIDER);
+    // Add brightness parameter for led_1 & inbuilt_led_2
+    Param brightness("Brightness", ESP_RMAKER_PARAM_BRIGHTNESS, value(DEFAULT_DUTY_CYCLE), PROP_FLAG_READ | PROP_FLAG_WRITE | PROP_FLAG_PERSIST);  // Or we can use a standard param addBrightnessParam(200)
+    brightness.addBounds(value(0), value(255), value(1));
+    brightness.addUIType(ESP_RMAKER_UI_SLIDER);
 
-  // Add Ambient Light Sensor paramter for ldr_sensor
+    // Add Ambient Light Sensor paramter for ldr_sensor
 
-  ldr_sensor->addNameParam();
-  ldr_sensor->assignPrimaryParam(ldr_sensor->getParamByName(ESP_RMAKER_DEF_POWER_NAME));
-  Param sensor("Sensor", ESP_RMAKER_PARAM_TEMPERATURE, value(lux), PROP_FLAG_READ | PROP_FLAG_TIME_SERIES);
+    ldr_sensor->addNameParam();
+    ldr_sensor->assignPrimaryParam(ldr_sensor->getParamByName(ESP_RMAKER_DEF_POWER_NAME));
+    Param sensor("Sensor", ESP_RMAKER_PARAM_TEMPERATURE, value(lux), PROP_FLAG_READ | PROP_FLAG_TIME_SERIES);
 
-  led_1->addParam(brightness);
-  inbuilt_led_2->addParam(brightness);
-  ldr_sensor->addParam(sensor);
+    led_1->addParam(brightness);
+    inbuilt_led_2->addParam(brightness);
+    ldr_sensor->addParam(sensor);
 
 
-  // Add both light devices callback function
-  inbuilt_led_2->addCb(write_callback);
-  led_1->addCb(write_callback);
+    // Add both light devices callback function
+    inbuilt_led_2->addCb(write_callback);
+    led_1->addCb(write_callback);
 
-  // Add devices to the node
-  my_node.addDevice(*led_1);
-  my_node.addDevice(*inbuilt_led_2);
-  my_node.addDevice(*ldr_sensor);
+    // Add devices to the node
+    my_node.addDevice(*led_1);
+    my_node.addDevice(*inbuilt_led_2);
+    my_node.addDevice(*ldr_sensor);
 
-  // This is optional
-  RMaker.enableOTA(OTA_USING_TOPICS);
-  // If you want to enable scheduling, set time zone for your region using
-  // setTimeZone(). The list of available values are provided here
-  // https://rainmaker.espressif.com/docs/time-service.html
-  // RMaker.setTimeZone("Asia/Shanghai");
-  //  Alternatively, enable the Timezone service and let the phone apps set the
-  //  appropriate timezone
-  RMaker.enableTZService();
+    // This is optional
+    RMaker.enableOTA(OTA_USING_TOPICS);
+    // If you want to enable scheduling, set time zone for your region using
+    // setTimeZone(). The list of available values are provided here
+    // https://rainmaker.espressif.com/docs/time-service.html
+    // RMaker.setTimeZone("Asia/Shanghai");
+    //  Alternatively, enable the Timezone service and let the phone apps set the
+    //  appropriate timezone
+    RMaker.enableTZService();
 
-  RMaker.enableSchedule();
+    RMaker.enableSchedule();
 
-  RMaker.enableScenes();
-  // Enable ESP Insights. Insteads of using the default http transport, this function will
-  // reuse the existing MQTT connection of Rainmaker, thereby saving memory space.
-  //initAppInsights();
+    RMaker.enableScenes();
+    // Enable ESP Insights. Insteads of using the default http transport, this function will
+    // reuse the existing MQTT connection of Rainmaker, thereby saving memory space.
+    //initAppInsights();
 
-  RMaker.enableSystemService(SYSTEM_SERV_FLAGS_ALL, 2, 2, 2);
+    RMaker.enableSystemService(SYSTEM_SERV_FLAGS_ALL, 2, 2, 2);
 
-  RMaker.start();
+    RMaker.start();
 
-  WiFi.onEvent(sysProvEvent);
-  WiFiProv.beginProvision(NETWORK_PROV_SCHEME_BLE, NETWORK_PROV_SCHEME_HANDLER_FREE_BTDM,
-                          NETWORK_PROV_SECURITY_1, pop, service_name);
+    WiFi.onEvent(sysProvEvent);
+    WiFiProv.beginProvision(NETWORK_PROV_SCHEME_BLE, NETWORK_PROV_SCHEME_HANDLER_FREE_BTDM,
+                            NETWORK_PROV_SECURITY_1, pop, service_name);
 }
 
 void loop() {
-  if (ARDUINO_EVENT_PROV_CRED_SUCCESS) {
-    lux = lux_handler(ldr_sensor_pin);
+    if (ARDUINO_EVENT_PROV_CRED_SUCCESS) {
+        lux = lux_handler(ldr_sensor_pin);
 
-    if (millis() - lastReportTime >= reportInterval) {
-      lastReportTime = millis();
-      ldr_sensor->updateAndReportParam("Sensor", lux);
-    }
-
-    if (digitalRead(inbuilt_switch) == LOW) {  // Push button pressed
-      // Key debounce handling
-      delay(100);
-      int startTime = millis();
-      while (digitalRead(inbuilt_switch) == LOW) {
-        delay(50);
-      }
-      int endTime = millis();
-
-      if ((endTime - startTime) > 7000) {
-        // If key pressed for more than 7secs, reset all
-        Serial.printf("Reset to factory.\n");
-        RMakerFactoryReset(2);
-      } else if ((endTime - startTime) > 3000) {
-        Serial.printf("Reset Wi-Fi.\n");
-        // If key pressed for more than 3secs, but less than 10, reset Wi-Fi
-        RMakerWiFiReset(2);
-      } else {
-        // Toggle switch state;
-        switch_state_2 = !switch_state_2;
-        Serial.printf("Toggle State to %s.\n", switch_state_2 ? "true" : "false");
-        if (inbuilt_led_2) {
-          inbuilt_led_2->updateAndReportParam(ESP_RMAKER_DEF_POWER_NAME,
-                                              switch_state_2);
+        if (millis() - lastReportTime >= reportInterval) {
+            lastReportTime = millis();
+            ldr_sensor->updateAndReportParam("Sensor", lux);
         }
-        (switch_state_2 == false) ? rgbLedWrite(inbuilt_led_pin, 0, 0, 0)
-                                  : rgbLedWrite(inbuilt_led_pin, 60, 60, 60);
+
+        if (digitalRead(inbuilt_switch) == LOW) {  // Push button pressed
+            // Key debounce handling
+            delay(100);
+            int startTime = millis();
+            while (digitalRead(inbuilt_switch) == LOW) {
+            delay(50);
+        }
+        int endTime = millis();
+
+        if ((endTime - startTime) > 7000) {
+            // If key pressed for more than 7secs, reset all
+            Serial.printf("Reset to factory.\n");
+            RMakerFactoryReset(2);
+        } else if ((endTime - startTime) > 3000) {
+            Serial.printf("Reset Wi-Fi.\n");
+            // If key pressed for more than 3secs, but less than 10, reset Wi-Fi
+            RMakerWiFiReset(2);
+        } else {
+            // Toggle switch state;
+            switch_state_2 = !switch_state_2;
+            Serial.printf("Toggle State to %s.\n", switch_state_2 ? "true" : "false");
+            if (inbuilt_led_2) {
+              inbuilt_led_2->updateAndReportParam(ESP_RMAKER_DEF_POWER_NAME,
+                                                  switch_state_2);
+            }
+            (switch_state_2 == false) ? rgbLedWrite(inbuilt_led_pin, 0, 0, 0)
+                                      : rgbLedWrite(inbuilt_led_pin, 60, 60, 60);
+        }
       }
     }
-  }
 }
